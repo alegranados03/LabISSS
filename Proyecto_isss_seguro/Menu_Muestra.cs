2c3ad55da33a69768bf1f7e05d4849b2f888a1e4
kajadava <carlos.vinse715@gmail.com>
2018-07-03 23:20:27 -0600

    prueba2

--- Proyecto_isss_seguro/Menu_Muestra.cs
+++ Proyecto_isss_seguro/Menu_Muestra.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Proyecto_isss_seguro
 {
-    public partial class Form1 : Form
+    public partial class Menu_Muestra : Form
     {
-        public Form1()
+        public Menu_Muestra()
         {
             InitializeComponent();
         }
